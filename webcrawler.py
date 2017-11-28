fa5739dae413360fba1eb060f407d7a968754435
dmarchewka <dmarchewka85@gmail.com>
2017-11-28 21:27:23 +0100

    renaming methods, adding sitemap printout

--- webcrawler.py
+++ webcrawler.py
@@ -3,16 +3,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.thm-troisdorf.de/'
+url = 'https://www.xml-sitemaps.com'
 
 class WebCrawler():
 
     _base_url = ''
-    _site_map = {}
+    _site_map = ''
     _links = set()
 
     def __init__(self, url):
-        self._base_url = str(url).rstrip('/')
+        self._base_url = url
+        if not str(self._base_url).endswith('/'):
+            self._base_url += '/'
 
     def _get_links(self, url):
 
@@ -23,23 +25,32 @@ class WebCrawler():
 
         soup = BeautifulSoup(req.text, "html.parser")
         for item in soup.find_all('a'):
-            href = str(item.get('href'))
-            if href and href.startswith('/'):
+            href = str(item.get('href')).replace(self._base_url, '')
+            if href and not href.startswith('http') and href.find('java') == -1:
                 links.append(href)
         return links
 
-    def _crawl_url(self, urls):
+    def _search_site_map(self, urls):
         for url in urls:
             if url not in self._links:
                 self._links.add(url)
                 links = self._get_links(url)
-                self._crawl_url(links)
+                for link in links:
+                    if 'sitemap.xml' in link:
+                        self._site_map = self._base_url +  link
+                        return
+                self._search_site_map(links)
 
-    def parse(self):
-        self._site_map = self._crawl_url(['/'])
-        for link in self._links:
-            print link
+    def get_site_map(self):
+        self._search_site_map(['/'])
+        if self._site_map:
+            req = requests.get(self._site_map)
 
+            if req.status_code != 200:
+                print 'Site map cannot be found %s' % req.status_code
+                return
+
+            print req.text
 
 wc = WebCrawler(url)
-wc.parse()
+wc.get_site_map()
